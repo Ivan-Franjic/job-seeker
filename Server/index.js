f5713f90147a9b8f68af375b83bd29cd84a3195c
Franke266 <ifranjic099@gmail.com>
2023-05-25 12:23:25 +0200

    bug fixed

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,12 +9,12 @@ import jobRoutes from "./Routes/jobs.js";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/jobs", jobRoutes);
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5001;
